6be9f22e6fcb67fdd2bd1eb7d8d085357892b89d
Eric Bosch <ebosch@twonav.com>
2018-12-18 12:07:47 +0100

    wip

--- update_config.sh
+++ update_config.sh
@@ -4,4 +4,5 @@ make ARCH=arm xconfig
 make ARCH=arm savedefconfig
 cp arch/arm/configs/imx6ul-var-dart-twonav_defconfig arch/arm/configs/imx6ul-var-dart-twonav_defconfig.orig
 cp defconfig arch/arm/configs/imx6ul-var-dart-twonav_defconfig
+rm -rf defconfig
 
