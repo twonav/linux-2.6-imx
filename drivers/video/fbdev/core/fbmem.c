fb36834815e3c226b544953b7b13f3fd68112f2f
Theodoros Paschidis <tpaschidis@twonav.com>
2020-04-22 18:00:46 +0200

    TWON-16900: remove CONFIG_TOWNAV_XXX and read module parameters

--- drivers/video/fbdev/core/fbmem.c
+++ drivers/video/fbdev/core/fbmem.c
@@ -664,8 +664,8 @@ int fb_show_logo(struct fb_info *info, int rotate)
 {
 	int y;
 
-	y = fb_show_logo_line(info, rotate, fb_logo.logo, 0,
-			      num_online_cpus());
+	int n_repeat = 1; // num_online_cpus()
+	y = fb_show_logo_line(info, rotate, fb_logo.logo, 0, n_repeat);
 	y = fb_show_extra_logos(info, y, rotate);
 
 	return y;
--- include/linux/linux_logo.h
+++ include/linux/linux_logo.h
@@ -41,7 +41,6 @@ extern const struct linux_logo logo_flasher_small_clut224;
 extern const struct linux_logo logo_twonav_trail_aventura_clut224;
 extern const struct linux_logo logo_twonav_crosstop_clut224;
 extern const struct linux_logo logo_os_trail_aventura_clut224;
-extern const struct linux_logo logo_os_crosstop_clut224;
 extern const struct linux_logo logo_blackfin_vga16;
 extern const struct linux_logo logo_blackfin_clut224;
 extern const struct linux_logo logo_dec_clut224;
