4fa2ed78753512052a77a7ee4e584e7ad6956190
David <dnieto@twonav.com>
2021-12-17 13:06:18 +0100

    TWON-17671: (VAR02) Add NXP Driver build

---
+++ update_config.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# -e  Exit immediately if a command exits with a non-zero status.
+set -e
+
+SCRIPT_NAME=${0##*/}
+
+DEFCONFIG_PREFIX="imx6ul-var-dart-"
+DEFCONFIG_SUFFIX="_defconfig"
+DEFCONFIG_ORIG_SUFFIX=".orig"
+PARAM_DEVICE_TYPE="twonav-2018"
+
+### usage ###
+function usage() {
+    echo "Usage:"
+	echo " ./${SCRIPT_NAME}"
+}
+
+###### parse input arguments ##
+readonly SHORTOPTS="h:t:"
+readonly LONGOPTS="help,type:"
+
+ARGS=$(getopt -s bash --options ${SHORTOPTS}  \
+  --longoptions ${LONGOPTS} --name ${SCRIPT_NAME} -- "$@" )
+
+eval set -- "$ARGS"
+
+while true; do
+	case $1 in		
+		-h|--help ) # get help
+			usage
+			exit 0;
+			;;
+		-- )
+			shift
+			break
+			;;
+		* )
+			shift
+			break
+			;;
+	esac
+	shift
+done
+
+#readonly DEFCONFIG="$DEFCONFIG_PREFIX$PARAM_DEVICE_TYPE$DEFCONFIG_SUFFIX"
+readonly DEFCONFIG="imx6ul-var-dart_defconfig"
+
+# Clean
+make ARCH=arm mrproper
+# Choose defconfig
+make ARCH=arm $DEFCONFIG
+# Modify defconfig
+make ARCH=arm xconfig QT_X11_NO_MITSHM=1
+make ARCH=arm savedefconfig
+# Backup original defconfig
+cp arch/arm/configs/$DEFCONFIG arch/arm/configs/$DEFCONFIG$DEFCONFIG_ORIG_SUFFIX
+# Apply changes
+cp defconfig arch/arm/configs/$DEFCONFIG
+# Delete current config
+rm -rf defconfig
+
